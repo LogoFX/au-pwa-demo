718476bdbb623900a13f279d92e69ed9277311d1
Dmitry Bublik <dmitry.buba@gmail.com>
2020-06-15 22:47:34 +0400

    chore: add 'Get' method to Contacts controller

--- server/Pwa.Server.Api/Controllers/ContactsController.cs
+++ server/Pwa.Server.Api/Controllers/ContactsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Pwa.Server.Domain.Services;
 
 namespace Pwa.Server.Api.Controllers
@@ -10,14 +12,34 @@ namespace Pwa.Server.Api.Controllers
     public class ContactsController : Controller
     {
         private readonly IContactService _contactsService;
+        private readonly IUsersService _usersService;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="T:Pwa.Server.Api.Controllers.ContactsController"/> class.
         /// </summary>
         /// <param name="contactsService">Contacts service.</param>
-        public ContactsController(IContactService contactsService)
+        /// <param name="usersService">Users service.</param>
+        public ContactsController(IContactService contactsService, IUsersService usersService)
         {
             _contactsService = contactsService;
+            _usersService = usersService;
+        }
+
+        /// <summary>
+        /// Gets all users.
+        /// </summary>
+        /// <returns>The list of users.</returns>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                return Ok(await _usersService.GetUsersAsync());
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult($"Unable to retrieve users: {ex.Message}");
+            }
         }
     }
 }
